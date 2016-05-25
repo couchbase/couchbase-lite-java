6a4afb361968d9ddec095d4101da4e1bcc394004
hideki <hideki@couchbase.com>
2016-05-25 12:39:20 -0700

    Eliminated HttpClient dependency and other unused dependencies.

--- src/main/java/com/couchbase/lite/storage/JavaSQLiteStorageEngine.java
+++ src/main/java/com/couchbase/lite/storage/JavaSQLiteStorageEngine.java
@@ -1,7 +1,7 @@
 /**
  * Created by Pasin Suriyentrakorn.
  *
- * Copyright (c) 2015 Couchbase, Inc. All rights reserved.
+ * Copyright (c) 2016 Couchbase, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -13,7 +13,6 @@
  * either express or implied. See the License for the specific language governing permissions
  * and limitations under the License.
  */
-
 package com.couchbase.lite.storage;
 
 import com.couchbase.lite.internal.database.DatabasePlatformSupport;
