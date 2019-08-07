4379755eeba7b2f1f474f90fceeb25b21102d381
Pasin Suriyentrakorn <pasin@couchbase.com>
2019-08-07 11:51:46 -0700

    Make prebuild_windows.bat accept VS Generator parameter
    The command now is prebuild_windows.bat <VS Generator Year: 2015, 2017 or 2019>

--- scripts/prebuild_windows.bat
+++ scripts/prebuild_windows.bat
@@ -1,7 +1,25 @@
+@echo off
+
+if "%1%" == "" (
+    echo Usage: prebuild_windows.bat ^<VS Generator: 2015,2017,2019^>
+    exit /B 1
+)
+
+if "%1%" == "2015" SET VS_GEN="Visual Studio 14 2015 Win64"
+if "%1%" == "2017" SET VS_GEN="Visual Studio 15 2017 Win64"
+if "%1%" == "2019" SET VS_GEN="Visual Studio 16 2019"
+
+if %VS_GEN% == "" (
+    echo Using an invalid VS Generator
+    exit /B 1
+)
+
 pushd ..\couchbase-lite-core\build_cmake
 mkdir x64
 pushd x64
-"C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 14 2015 Win64" ..\..
+
+echo Build using %VS_GEN% ...
+"C:\Program Files\CMake\bin\cmake.exe" -G %VS_GEN% ..\..
 "C:\Program Files\CMake\bin\cmake.exe" --build . --target LiteCore --config RelWithDebInfo
 popd
 popd
