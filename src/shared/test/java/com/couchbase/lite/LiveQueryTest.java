couchbase/couchbase-lite-java

commit 49d905de9897710a3fac11efe712d747c02be422

Fix CBL-172: There should be no initial delay when starting a liveQuery
Change-Id: I0706bf9222b1001b8c573dbf65ef1328036a0095

src/shared/main/java/com/couchbase/lite/LiveQuery.java
@@ -82,7 +82,7 @@ final class LiveQuery implements DatabaseChangeListener {
     //---------------------------------------------
 
     @Override
-    public void changed(@NonNull DatabaseChange change) { update(); }
+    public void changed(@NonNull DatabaseChange change) { update(LIVE_QUERY_UPDATE_INTERVAL_MS); }
 
     //---------------------------------------------
     // protected methods
@@ -135,13 +135,13 @@ final class LiveQuery implements DatabaseChangeListener {
                 // Here if the live query was already running.  This can happen in two ways:
                 // 1) when adding another listener
                 // 2) when the query parameters have changed.
-                // In either case we may want to kick off a new query.
+                // In either case we probably want to kick off a new query.
                 // In the latter case the current query results are irrelevant and need to be cleared.
                 if (shouldClearResults) { releaseResultSetSynchronized(); }
             }
         }
 
-        update();
+        update(0);
     }
 
     //---------------------------------------------
@@ -164,9 +164,9 @@ final class LiveQuery implements DatabaseChangeListener {
         }
     }
 
-    private void update() {
+    private void update(long delay) {
         if (!state.compareAndSet(State.STARTED, State.SCHEDULED)) { return; }
-        query.getDatabase().scheduleOnQueryExecutor(this::refreshResults, LIVE_QUERY_UPDATE_INTERVAL_MS);
+        query.getDatabase().scheduleOnQueryExecutor(this::refreshResults, delay);
     }
 
     // Runs on the query.database.queryExecutor

commit 099f1268258aeb3c742632f501ecd85b46787239

Fix UT broken in 49d905de989771
Change-Id: I36774a18bf168092cd7094fd11700b5bcfe89c40

src/shared/test/java/com/couchbase/lite/LiveQueryTest.java
@@ -86,7 +86,6 @@ public class LiveQueryTest extends BaseTest {
         query.removeChangeListener(token2);
     }
 
-    // Multiple changes in a short time should cause only a single update
     @Test
     public void testLiveQueryDelay() throws CouchbaseLiteException, InterruptedException {
         query = QueryBuilder
@@ -103,13 +102,18 @@ public class LiveQueryTest extends BaseTest {
         createDocNumbered(12);
         createDocNumbered(13);
         createDocNumbered(14);
+        createDocNumbered(15);
+        createDocNumbered(16);
 
         // Ya, I know...
         Thread.sleep(1000);
 
-        assertEquals(1, value.get());
-
         query.removeChangeListener(token);
+
+        // There should be two callbacks:
+        //  - immediately on registration
+        //  - after LIVE_QUERY_UPDATE_INTERVAL_MS when the change gets noticed.
+        assertEquals(2, value.get());
     }
 
     // Changing query parameters should cause an update.
@@ -120,7 +124,8 @@ public class LiveQueryTest extends BaseTest {
         query = QueryBuilder
             .select(SelectResult.expression(Meta.id))
             .from(DataSource.database(db))
-            .where(Expression.property("number1").greaterThanOrEqualTo(Expression.parameter("VALUE")))
+            .where(Expression.property("number1")
+                .greaterThanOrEqualTo(Expression.parameter("VALUE")))
             .orderBy(Ordering.property("number1").ascending());
         Parameters params = new Parameters();
         params.setInt("VALUE", 2);
@@ -185,7 +190,8 @@ public class LiveQueryTest extends BaseTest {
         return QueryBuilder
             .select(SelectResult.expression(Meta.id))
             .from(DataSource.database(db))
-            .where(Expression.property("number1").greaterThanOrEqualTo(Expression.intValue(value.intValue())))
+            .where(Expression.property("number1")
+                .greaterThanOrEqualTo(Expression.intValue(value.intValue())))
             .orderBy(Ordering.property("number1").ascending());
     }
 