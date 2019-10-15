couchbase/couchbase-lite-java

commit d3b32ccbf5819e55b116b60a7e25374b29c13f3c

Rationalize logging
Change-Id: I3591db2a9193bcb2c1c1041f5d61de5a04291d3c

src/shared/main/java/com/couchbase/lite/AbstractConsoleLogger.java
@@ -24,15 +24,40 @@ import java.util.EnumSet;
 import com.couchbase.lite.internal.core.C4Log;
 import com.couchbase.lite.internal.utils.Preconditions;
 
+
 /**
  * The base console logger class.
  */
 abstract class AbstractConsoleLogger implements Logger {
+    private EnumSet<LogDomain> logDomains = LogDomain.ALL_DOMAINS;
     private LogLevel logLevel = LogLevel.WARNING;
-    private EnumSet<LogDomain> logDomains = EnumSet.of(LogDomain.ALL);
 
     // Singleton instance accessible from Log.getConsole()
     AbstractConsoleLogger() { }
+
+    @Override
+    public void log(@NonNull LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
+        if ((level.compareTo(logLevel) < 0) || (!logDomains.contains(domain))) { return; }
+        doLog(level, domain, message);
+    }
+
+    @NonNull
+    @Override
+    public LogLevel getLevel() { return logLevel; }
+
+    /**
+     * Sets the overall logging level that will be written to the console log.
+     *
+     * @param level The lowest (most verbose) level to include in the logs
+     */
+    public void setLevel(@NonNull LogLevel level) {
+        Preconditions.checkArgNotNull(level, "level");
+
+        if (logLevel == level) { return; }
+
+        logLevel = level;
+        C4Log.setCallbackLevel(logLevel);
+    }
 
     /**
      * Gets the domains that will be considered for writing to the console log.
@@ -49,47 +74,9 @@ abstract class AbstractConsoleLogger implements Logger {
      */
     public void setDomains(@NonNull EnumSet<LogDomain> domains) {
         Preconditions.checkArgNotNull(domains, "domains");
-        logDomains = domains;
-    }
-
-    private void setCallbackLevel(@NonNull LogLevel level) {
-        Preconditions.checkArgNotNull(level, "level");
-
-        LogLevel callbackLevel = level;
-        final Logger custom = Database.log.getCustom();
-        if ((custom != null) && (custom.getLevel().compareTo(callbackLevel) < 0)) {
-            callbackLevel = custom.getLevel();
-        }
-
-        C4Log.setCallbackLevel(callbackLevel.getValue());
-    }
-
-    @NonNull
-    @Override
-    public LogLevel getLevel() { return logLevel; }
-
-    /**
-     * Sets the overall logging level that will be written to the console log.
-     *
-     * @param level The maximum level to include in the logs
-     */
-    public void setLevel(@NonNull LogLevel level) {
-        Preconditions.checkArgNotNull(level, "level");
-
-        if (logLevel == level) { return; }
-
-        logLevel = level;
-        setCallbackLevel(level);
-    }
-
-    @Override
-    public void log(@NonNull LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
-        if (level.compareTo(logLevel) < 0
-                || (!logDomains.contains(domain)
-                && !logDomains.contains(LogDomain.ALL))) {
-            return;
-        }
-        doLog(level, domain, message);
+        logDomains = (!domains.contains(LogDomain.ALL))
+            ? domains
+            : LogDomain.ALL_DOMAINS;
     }
 
     protected abstract void doLog(LogLevel level, @NonNull LogDomain domain, @NonNull String message);

src/shared/main/java/com/couchbase/lite/FileLogger.java
@@ -18,14 +18,10 @@
 package com.couchbase.lite;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-
-import com.couchbase.lite.internal.core.C4Constants;
 import com.couchbase.lite.internal.core.C4Log;
 import com.couchbase.lite.internal.core.CBLVersion;
 import com.couchbase.lite.internal.support.Log;
@@ -38,13 +34,40 @@ import com.couchbase.lite.internal.support.Log;
  * a separate file.
  */
 public final class FileLogger implements Logger {
-    private static final LogDomain DOMAIN = com.couchbase.lite.LogDomain.DATABASE;
-    private final HashMap<LogDomain, String> domainObjects = new HashMap<>();
-    private LogLevel logLevel = com.couchbase.lite.LogLevel.INFO;
     private LogFileConfiguration config;
+    private LogLevel logLevel = LogLevel.WARNING;
 
-    // Singleton instance accessible from Log.getConsole()
-    FileLogger() { setupDomainObjects(); }
+    // The singleton instance is available from Database.log.getFile()
+    FileLogger() { }
+
+    @Override
+    public void log(@NonNull LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
+        if (level.compareTo(logLevel) < 0) { return; }
+        C4Log.log(Log.getC4DomainForLoggingDomain(domain), level.getValue(), message);
+    }
+
+    @NonNull
+    @Override
+    public LogLevel getLevel() {
+        logLevel = Log.getLogLevelForC4Level(C4Log.getBinaryFileLevel());
+        return logLevel;
+    }
+
+    /**
+     * Sets the overall logging level that will be written to the logging files.
+     *
+     * @param level The maximum level to include in the logs
+     */
+    public void setLevel(@NonNull LogLevel level) {
+        if (config == null) {
+            throw new IllegalStateException("Cannot set logging level before setting the configuration");
+        }
+
+        if (logLevel == level) { return; }
+
+        logLevel = level;
+        C4Log.setBinaryFileLevel(level.getValue());
+    }
 
     /**
      * Gets the configuration currently in use on the file logger.
@@ -62,73 +85,42 @@ public final class FileLogger implements Logger {
      *
      * @param config The configuration to use
      */
-    public void setConfig(LogFileConfiguration config) {
-        this.config = config == null ? null : config.readOnlyCopy();
+    public void setConfig(@Nullable LogFileConfiguration config) {
         if (config == null) {
-            Log.w(
-                DOMAIN,
-                "Database.log.getFile().getConfig() is now null, meaning file logging is disabled.  "
-                    + "Log files required for product support are not being generated.");
+            setNullConfig();
+            return;
         }
 
-        updateConfig();
-    }
+        this.config = config.readOnlyCopy();
 
-    private void setupDomainObjects() {
-        domainObjects.put(com.couchbase.lite.LogDomain.DATABASE, C4Constants.LogDomain.DATABASE);
-        domainObjects.put(com.couchbase.lite.LogDomain.QUERY, C4Constants.LogDomain.QUERY);
-        domainObjects.put(com.couchbase.lite.LogDomain.REPLICATOR, C4Constants.LogDomain.SYNC);
-        for (Map.Entry<LogDomain, String> entry : domainObjects.entrySet()) {
-            C4Log.setLevel(entry.getValue(), C4Constants.LogLevel.DEBUG);
+        if (!new File(config.getDirectory()).mkdir()) {
+            Log.w(LogDomain.DATABASE, "Cannot create log file!");
         }
+
+        C4Log.writeToBinaryFile(
+            config.getDirectory(),
+            LogLevel.INFO.getValue(),
+            config.getMaxRotateCount(),
+            config.getMaxSize(),
+            config.usesPlaintext(),
+            CBLVersion.getVersionInfo());
     }
 
-    @SuppressFBWarnings("RV_RETURN_VALUE_IGNORED_BAD_PRACTICE")
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private void updateConfig() {
-        if (config != null) {
-            new File(config.getDirectory()).mkdir();
-            C4Log.writeToBinaryFile(
-                config.getDirectory(),
-                logLevel.getValue(),
-                config.getMaxRotateCount(),
-                config.getMaxSize(),
-                config.usesPlaintext(),
-                CBLVersion.getVersionInfo());
-        }
-        else {
-            C4Log.writeToBinaryFile(
-                null,
-                logLevel.getValue(),
-                1,
-                1024 * 500,
-                false,
-                CBLVersion.getVersionInfo());
-        }
-    }
+    private void setNullConfig() {
+        Log.w(
+            LogDomain.DATABASE,
+            "Database.log.getFile().getConfig() is now null and file logging is disabled.  "
+                + "The log files *required* for product support are not being generated.");
 
-    @NonNull
-    @Override
-    public LogLevel getLevel() { return com.couchbase.lite.LogLevel.values()[C4Log.getBinaryFileLevel()]; }
+        this.config = null;
 
-    /**
-     * Sets the overall logging level that will be written to
-     * the logging files.
-     *
-     * @param level The maximum level to include in the logs
-     */
-    public void setLevel(@NonNull LogLevel level) {
-        if (config == null) { throw new IllegalStateException("Cannot set logging level without a configuration"); }
-
-        if (logLevel.equals(level)) { return; }
-
-        logLevel = level;
-        C4Log.setBinaryFileLevel(level.getValue());
-    }
-
-    @Override
-    public void log(@NonNull LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
-        if (level.compareTo(logLevel) < 0 || !domainObjects.containsKey(domain)) { return; }
-        C4Log.log(domainObjects.get(domain), level.getValue(), message);
+        C4Log.writeToBinaryFile(
+            null,
+            LogLevel.INFO.getValue(),
+            1,
+            1024 * 500,
+            false,
+            CBLVersion.getVersionInfo());
     }
 }
+

src/shared/test/java/com/couchbase/lite/LogTest.java
@@ -456,7 +456,7 @@ public class LogTest extends BaseTest {
         LogTestLogger customLogger = new LogTestLogger();
         customLogger.setLevel(LogLevel.VERBOSE);
         Database.log.setCustom(customLogger);
-        Database.log.getConsole().setDomains(EnumSet.of(LogDomain.ALL));
+        Database.log.getConsole().setDomains(LogDomain.ALL_DOMAINS);
         Database.log.getConsole().setLevel(LogLevel.VERBOSE);
 
         String hebrew = "מזג האוויר נחמד היום"; // The weather is nice today.
@@ -481,18 +481,23 @@ public class LogTest extends BaseTest {
 
     //region Helper methods
     private void testWithConfiguration(LogLevel level, LogFileConfiguration config, Runnable r) {
-        LogFileConfiguration old = Database.log.getFile().getConfig();
-        EnumSet<LogDomain> domains = Database.log.getConsole().getDomains();
-        Database.log.getFile().setConfig(config);
-        Database.log.getFile().setLevel(level);
-        try {
-            r.run();
-        }
+        final ConsoleLogger consoleLogger = Database.log.getConsole();
+        final EnumSet<LogDomain> consoleDomains = consoleLogger.getDomains();
+        final LogLevel consoleLevel = consoleLogger.getLevel();
+        consoleLogger.setLevel(level);
+
+        final FileLogger fileLogger = Database.log.getFile();
+        final LogFileConfiguration fileConfig = fileLogger.getConfig();
+        final LogLevel fileLevel = fileLogger.getLevel();
+        fileLogger.setConfig(config);
+        fileLogger.setLevel(level);
+
+        try { r.run(); }
         finally {
-            Database.log.getFile().setLevel(LogLevel.INFO);
-            Database.log.getFile().setConfig(old);
-            Database.log.getConsole().setDomains(domains);
-            Database.log.getConsole().setLevel(LogLevel.WARNING);
+            consoleLogger.setDomains(consoleDomains);
+            consoleLogger.setLevel(consoleLevel);
+            fileLogger.setLevel(fileLevel);
+            fileLogger.setConfig(fileConfig);
         }
     }
 

src/shared/main/java/com/couchbase/lite/AbstractDatabase.java
@@ -24,6 +24,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -76,7 +77,6 @@ abstract class AbstractDatabase {
     @SuppressWarnings("ConstantName")
     @NonNull
     public static final com.couchbase.lite.Log log = new com.couchbase.lite.Log();
-    static { Log.setLogLevel(LogDomain.ALL, LogLevel.WARNING); }
 
     //---------------------------------------------
     // Constants
@@ -183,16 +183,20 @@ abstract class AbstractDatabase {
      * @param domain The log domain
      * @param level  The log level
      * @deprecated As of 2.5 because it is being replaced with the
-     * {@link com.couchbase.lite.Log#getConsole() getConsole} method
-     * from the {@link #log log} property.  This method has
-     * been replaced with a no-op to preserve API compatibility.
+     * {@link com.couchbase.lite.Log#getConsole() getConsole} method from the {@link #log log} property.
+     * This method will eventually be replaced with a no-op to preserve API compatibility.
+     * Until it is, its interactions with other logging maybe be fairly unpredictable.
      */
     @Deprecated
     public static void setLogLevel(@NonNull LogDomain domain, @NonNull LogLevel level) {
         Preconditions.checkArgNotNull(domain, "domain");
         Preconditions.checkArgNotNull(level, "level");
 
-        Log.setLogLevel(domain, level);
+        final EnumSet<LogDomain> domains = (domain == LogDomain.ALL)
+            ? LogDomain.ALL_DOMAINS
+            : EnumSet.of(domain);
+
+        Log.setC4LogLevel(domains, level);
     }
 
     private static File getDatabasePath(File dir, String name) {

src/shared/main/java/com/couchbase/lite/Log.java
@@ -19,8 +19,6 @@ package com.couchbase.lite;
 
 import android.support.annotation.NonNull;
 
-import com.couchbase.lite.internal.core.C4Log;
-
 
 /**
  * Gets the log controller for Couchbase Lite, which stores the
@@ -36,8 +34,8 @@ public final class Log {
 
     private Logger customLogger;
 
-    // Singleton instance accessible from Database.log
-    Log() { C4Log.setCallbackLevel(LogLevel.WARNING.getValue()); }
+    // The singleton instance is available from Database.log
+    Log() { com.couchbase.lite.internal.support.Log.initLogging(BuildConfig.DEBUG); }
 
     /**
      * Gets the logger that writes to the Android system log

src/shared/main/java/com/couchbase/lite/LogDomain.java
@@ -17,9 +17,18 @@
 //
 package com.couchbase.lite;
 
+import java.util.EnumSet;
+
+
 /**
  * Log domain
  */
 public enum LogDomain {
-    ALL, DATABASE, QUERY, REPLICATOR, NETWORK
+    /**
+     * @deprecated redundant and confusing
+     */
+    @Deprecated ALL,
+    DATABASE, QUERY, REPLICATOR, NETWORK;
+
+    public static final EnumSet<LogDomain> ALL_DOMAINS = EnumSet.of(DATABASE, QUERY, REPLICATOR, NETWORK);
 }

src/shared/main/java/com/couchbase/lite/LogLevel.java
@@ -17,7 +17,8 @@
 //
 package com.couchbase.lite;
 
-import com.couchbase.lite.internal.support.Log;
+import com.couchbase.lite.internal.core.C4Constants;
+
 
 /**
  * Log level.
@@ -26,53 +27,57 @@ public enum LogLevel {
     /**
      * Debug log messages. Only present in debug builds of CouchbaseLite.
      */
-    DEBUG(Log.C4LOG_DEBUG),
+    DEBUG(C4Constants.LogLevel.DEBUG),
 
     /**
      * Verbose log messages.
      */
-    VERBOSE(Log.C4LOG_VERBOSE),
+    VERBOSE(C4Constants.LogLevel.VERBOSE),
 
     /**
      * Informational log messages.
      */
-    INFO(Log.C4LOG_INFO),
+    INFO(C4Constants.LogLevel.INFO),
 
     /**
      * Warning log messages.
      */
-    WARNING(Log.C4LOG_WARN),
+    WARNING(C4Constants.LogLevel.WARNING),
 
     /**
      * Error log messages. These indicate immediate errors that need to be addressed.
      */
-    ERROR(Log.C4LOG_ERROR),
+    ERROR(C4Constants.LogLevel.ERROR),
 
     /**
      * Disabling log messages of a given log domain.
      */
-    NONE(Log.C4LOG_NONE);
+    NONE(C4Constants.LogLevel.NONE);
+
 
     private final int value;
 
-    LogLevel(int value) {
-        this.value = value;
-    }
+    LogLevel(int value) { this.value = value; }
 
-    public int getValue() {
-        return value;
-    }
+    public int getValue() { return value; }
 
     @Override
     public String toString() {
-        switch(this) {
-            case DEBUG: return "D";
-            case VERBOSE: return "V";
-            case INFO: return "I";
-            case WARNING: return "W";
-            case ERROR: return "E";
-            case NONE: return "";
-            default: throw new IllegalArgumentException();
+        switch (this) {
+            case DEBUG:
+                return "D";
+            case VERBOSE:
+                return "V";
+            case INFO:
+                return "I";
+            case WARNING:
+                return "W";
+            case ERROR:
+                return "E";
+            case NONE:
+                return "";
+            default:
+                throw new IllegalArgumentException();
         }
     }
 }

src/shared/main/java/com/couchbase/lite/internal/AbstractExecutionService.java
@@ -140,9 +140,7 @@ public abstract class AbstractExecutionService implements ExecutionService {
 
     private final CloseableExecutor concurrentExecutor;
 
-    public AbstractExecutionService() {
-        concurrentExecutor = new ConcurrentExecutor(getThreadPoolExecutor());
-    }
+    public AbstractExecutionService() { concurrentExecutor = new ConcurrentExecutor(getThreadPoolExecutor()); }
 
     @NonNull
     public abstract Executor getThreadPoolExecutor();

commit e769dffad6ddbfbc3d7f4ecf13381992f01f3871

Fix bug introduced in d3b32ccbf5819e5: Normalize Logging
Change-Id: I489b222dac2613953296c6a9820f49e6e1d9706f

src/shared/main/java/com/couchbase/lite/AbstractConsoleLogger.java
@@ -30,11 +30,11 @@ import com.couchbase.lite.internal.utils.Preconditions;
  * The base console logger class.
  */
 abstract class AbstractConsoleLogger implements Logger {
-    private EnumSet<LogDomain> logDomains = LogDomain.ALL_DOMAINS;
-    private LogLevel logLevel = LogLevel.WARNING;
+    private EnumSet<LogDomain> logDomains;
+    private LogLevel logLevel;
 
     // Singleton instance accessible from Database.log.getConsole()
-    AbstractConsoleLogger() { }
+    AbstractConsoleLogger() { reset(); }
 
     @Override
     public void log(@NonNull LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
@@ -83,7 +83,7 @@ abstract class AbstractConsoleLogger implements Logger {
     protected abstract void doLog(LogLevel level, @NonNull LogDomain domain, @NonNull String message);
 
     @VisibleForTesting
-    void reset() {
+    final void reset() {
         logDomains = LogDomain.ALL_DOMAINS;
         logLevel = LogLevel.WARNING;
     }

src/shared/main/java/com/couchbase/lite/FileLogger.java
@@ -35,11 +35,12 @@ import com.couchbase.lite.internal.support.Log;
  * a separate file.
  */
 public final class FileLogger implements Logger {
+    @Nullable
     private LogFileConfiguration config;
-    private LogLevel logLevel = LogLevel.WARNING;
+    private LogLevel logLevel;
 
     // The singleton instance is available from Database.log.getFile()
-    FileLogger() { }
+    FileLogger() { reset(); }
 
     @Override
     public void log(@NonNull LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
@@ -61,7 +62,7 @@ public final class FileLogger implements Logger {
      */
     public void setLevel(@NonNull LogLevel level) {
         if (config == null) {
-            throw new IllegalStateException("Cannot set the file logger's level before setting its configuration");
+            throw new IllegalStateException("Cannot set the FileLogger's level while it has no configuration");
         }
 
         if (logLevel == level) { return; }
@@ -71,25 +72,31 @@ public final class FileLogger implements Logger {
     }
 
     /**
-     * Gets the configuration currently in use on the file logger.
-     * Note that once it is set, it can no longer be modified and doing so
-     * will throw an exception
+     * Gets the configuration currently in use by the file logger.
+     * Note that once a configuration has been installed in the logger,
+     * that configuration can no longer be modified.
+     * An attempt to modify the configuration returned by this method will cause an exception.
      *
      * @return The configuration currently in use
      */
     public LogFileConfiguration getConfig() { return config; }
 
     /**
-     * Sets the configuration currently to use on the file logger.
-     * Note that once it is set, it can no longer be modified and doing so
-     * will throw an exception
+     * Sets the configuration for use by the file logger.
      *
-     * @param config The configuration to use
+     * @param newConfig The configuration to use
      */
-    public void setConfig(@Nullable LogFileConfiguration config) {
-        if (this.config != null) { throw new IllegalStateException("Attempt to reset the file logger config"); }
+    public void setConfig(@Nullable LogFileConfiguration newConfig) {
+        if (newConfig == null) {
+            Log.w(
+                LogDomain.DATABASE,
+                "Database.log.getFile().getConfig() is now null: logging is disabled.  "
+                    + "Log files required for product support are not being generated.");
+            config = null;
+            return;
+        }
 
-        if (config == null) { return; }
+        config = newConfig.readOnlyCopy();
 
         final String logDirPath = config.getDirectory();
 
@@ -102,12 +109,11 @@ public final class FileLogger implements Logger {
             if (!logDir.isDirectory()) { errMsg = logDir.getAbsolutePath() + " is not a directory"; }
             else if (!logDir.canWrite()) { errMsg = logDir.getAbsolutePath() + " is not writable"; }
         }
+
         if (errMsg != null) {
             Log.w(LogDomain.DATABASE, errMsg);
             return;
         }
-
-        this.config = config.readOnlyCopy();
 
         C4Log.writeToBinaryFile(
             logDirPath,
@@ -120,8 +126,8 @@ public final class FileLogger implements Logger {
 
     @VisibleForTesting
     void reset() {
-        logLevel = LogLevel.WARNING;
         config = null;
+        logLevel = LogLevel.WARNING;
     }
 }
 

src/shared/main/java/com/couchbase/lite/LogFileConfiguration.java
@@ -13,12 +13,13 @@ public final class LogFileConfiguration {
     //---------------------------------------------
     // member variables
     //---------------------------------------------
+    private final boolean readonly;
+
     private final String directory;
     private int maxRotateCount = 1;
     private long maxSize = 1024 * 500;
 
 
-    private boolean readonly;
     private boolean usePlaintext;
 
     //---------------------------------------------
@@ -26,34 +27,12 @@ public final class LogFileConfiguration {
     //---------------------------------------------
 
     /**
-     * Constructs a file configuration object with the given directory
-     *
-     * @param directory The directory that the logs will be written to
-     */
-    public LogFileConfiguration(@NonNull String directory) {
-        if (directory == null) {
-            throw new IllegalArgumentException("directory cannot be null");
-        }
-
-        this.directory = directory;
-    }
-
-    /**
      * Constructs a file configuration object based on another one so
      * that it may be modified
      *
      * @param other The other configuration to copy settings from
      */
-    public LogFileConfiguration(@NonNull LogFileConfiguration other) {
-        if (other == null) {
-            throw new IllegalArgumentException("other cannot be null");
-        }
-
-        directory = other.directory;
-        maxRotateCount = other.maxRotateCount;
-        maxSize = other.maxSize;
-        usePlaintext = other.usePlaintext;
-    }
+    public LogFileConfiguration(@NonNull LogFileConfiguration other) { this(other, false); }
 
     /**
      * Constructs a file configuration object based on another one but changing
@@ -64,12 +43,42 @@ public final class LogFileConfiguration {
      */
     public LogFileConfiguration(@NonNull String directory, LogFileConfiguration other) {
         this(directory);
-        if (other != null) {
-            maxRotateCount = other.maxRotateCount;
-            maxSize = other.maxSize;
-            usePlaintext = other.usePlaintext;
-        }
+
+        if (other == null) { return; }
+        maxRotateCount = other.maxRotateCount;
+        maxSize = other.maxSize;
+        usePlaintext = other.usePlaintext;
     }
+
+    /**
+     * Constructs a file configuration object with the given directory
+     *
+     * @param directory The directory that the logs will be written to
+     */
+    public LogFileConfiguration(@NonNull String directory) {
+        if (directory == null) { throw new IllegalArgumentException("directory cannot be null"); }
+
+        this.directory = directory;
+        readonly = false;
+    }
+
+    /**
+     * Constructs a file configuration object based on another one so
+     * that it may be modified
+     *
+     * @param other The other configuration to copy settings from
+     */
+    private LogFileConfiguration(@NonNull LogFileConfiguration other, boolean readonly) {
+        if (other == null) { throw new IllegalArgumentException("other cannot be null"); }
+
+        directory = other.directory;
+        maxRotateCount = other.maxRotateCount;
+        maxSize = other.maxSize;
+        usePlaintext = other.usePlaintext;
+
+        this.readonly = readonly;
+    }
+
 
     //---------------------------------------------
     // Setters
@@ -98,9 +107,7 @@ public final class LogFileConfiguration {
      *
      * @return The number of rotated logs that are saved
      */
-    public int getMaxRotateCount() {
-        return maxRotateCount;
-    }
+    public int getMaxRotateCount() { return maxRotateCount; }
 
     /**
      * Sets the number of rotated logs that are saved (i.e.
@@ -129,9 +136,7 @@ public final class LogFileConfiguration {
      *
      * @return The max size of the log file in bytes
      */
-    public long getMaxSize() {
-        return maxSize;
-    }
+    public long getMaxSize() { return maxSize; }
 
     /**
      * Sets the max size of the log file in bytes.  If a log file
@@ -156,9 +161,7 @@ public final class LogFileConfiguration {
      *
      * @return Whether or not CBL is logging in plaintext
      */
-    public boolean usesPlaintext() {
-        return usePlaintext;
-    }
+    public boolean usesPlaintext() { return usePlaintext; }
 
     /**
      * Gets the directory that the logs files are stored in.
@@ -166,17 +169,11 @@ public final class LogFileConfiguration {
      * @return The directory that the logs files are stored in.
      */
     @NonNull
-    public String getDirectory() {
-        return directory;
-    }
+    public String getDirectory() { return directory; }
 
     //---------------------------------------------
     // Package level access
     //---------------------------------------------
 
-    LogFileConfiguration readOnlyCopy() {
-        final LogFileConfiguration config = new LogFileConfiguration(this);
-        config.readonly = true;
-        return config;
-    }
+    LogFileConfiguration readOnlyCopy() { return new LogFileConfiguration(this, true); }
 }

src/shared/test/java/com/couchbase/lite/LogTest.java
@@ -19,6 +19,7 @@ import org.junit.Test;
 import com.couchbase.lite.internal.core.CBLVersion;
 import com.couchbase.lite.internal.support.Log;
 
+import static com.couchbase.lite.utils.TestUtils.assertThrows;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -352,11 +353,9 @@ public class LogTest extends BaseTest {
         long maxSize = 2048;
         boolean usePlainText = true;
 
-        thrown.expect(IllegalArgumentException.class);
-        new LogFileConfiguration((String) null);
+        assertThrows(IllegalArgumentException.class, () -> new LogFileConfiguration((String) null));
 
-        thrown.expect(IllegalArgumentException.class);
-        new LogFileConfiguration((LogFileConfiguration) null);
+        assertThrows(IllegalArgumentException.class, () -> new LogFileConfiguration((LogFileConfiguration) null));
 
         LogFileConfiguration config = new LogFileConfiguration(tempDirPath)
             .setMaxRotateCount(rotateCount)
@@ -383,18 +382,28 @@ public class LogTest extends BaseTest {
             LogLevel.DEBUG,
             config,
             () -> {
-                thrown.expect(IllegalStateException.class);
-                Database.log.getFile().setConfig(config);
+                assertThrows(IllegalStateException.class, () -> Database.log.getFile().getConfig().setMaxSize(1024));
 
-                thrown.expect(IllegalStateException.class);
-                Database.log.getFile().getConfig().setMaxSize(1024);
+                assertThrows(
+                    IllegalStateException.class,
+                    () -> Database.log.getFile().getConfig().setMaxRotateCount(3));
 
-                thrown.expect(IllegalStateException.class);
-                Database.log.getFile().getConfig().setMaxRotateCount(3);
-
-                thrown.expect(IllegalStateException.class);
-                Database.log.getFile().getConfig().setUsePlaintext(true);
+                assertThrows(
+                    IllegalStateException.class,
+                    () -> Database.log.getFile().getConfig().setUsePlaintext(true));
             });
+    }
+
+    @Test
+    public void testSetNewLogFileConfiguration() throws Exception {
+        LogFileConfiguration config = new LogFileConfiguration(tempDirPath);
+
+        final FileLogger fileLogger = Database.log.getFile();
+        fileLogger.setConfig(config);
+        fileLogger.setConfig(null);
+        fileLogger.setConfig(null);
+        fileLogger.setConfig(config);
+        fileLogger.setConfig(new LogFileConfiguration(tempDirPath + "/foo"));
     }
 
     @Test