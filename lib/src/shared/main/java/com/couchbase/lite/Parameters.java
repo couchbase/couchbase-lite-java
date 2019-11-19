63eabc513bb2ebc993b15e6755fe7f96e56f80fd
G. Blake Meike <blake.meike@couchbase.com>
2019-11-19 13:48:38 -0800

    Cleanup Parameters
    Change-Id: Ifeefeab3af9f403ee996ea6dfd3855775cc96ba7

--- lib/src/shared/main/java/com/couchbase/lite/Parameters.java
+++ lib/src/shared/main/java/com/couchbase/lite/Parameters.java
@@ -18,6 +18,7 @@
 package com.couchbase.lite;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -25,6 +26,7 @@ import java.util.Map;
 
 import com.couchbase.lite.internal.fleece.AllocSlice;
 import com.couchbase.lite.internal.fleece.FLEncoder;
+import com.couchbase.lite.internal.utils.Preconditions;
 
 
 /**
@@ -35,18 +37,20 @@ public final class Parameters {
     //---------------------------------------------
     // member variables
     //---------------------------------------------
-
+    @NonNull
     private final Map<String, Object> map;
-    private boolean readonly;
+    private final boolean readonly;
 
     //---------------------------------------------
     // Constructors
     //---------------------------------------------
-
     public Parameters() { this(null); }
 
-    public Parameters(Parameters parameters) {
+    public Parameters(@Nullable Parameters parameters) { this(parameters, false); }
+
+    private Parameters(@Nullable Parameters parameters, boolean readonly) {
         map = (parameters == null) ? new HashMap<>() : new HashMap<>(parameters.map);
+        this.readonly = readonly;
     }
 
     //---------------------------------------------
@@ -54,18 +58,15 @@ public final class Parameters {
     //---------------------------------------------
 
     /**
-     * Set a value to the query parameter referenced by the given name. A query parameter
-     * is defined by using the Expression's parameter(String name) function.
+     * Gets a parameter's value.
      *
-     * @param name  The parameter name.
-     * @param value The value.
-     * @return The self object.
+     * @param name The parameter name.
+     * @return The parameter value.
      */
-    public Parameters setValue(@NonNull String name, Object value) {
-        if (name == null) { throw new IllegalArgumentException("name cannot be null."); }
-        if (readonly) { throw new IllegalStateException("Parameters is readonly mode."); }
-        map.put(name, value);
-        return this;
+    @Nullable
+    public Object getValue(@NonNull String name) {
+        Preconditions.checkArgNotNull(name, "name");
+        return map.get(name);
     }
 
     /**
@@ -77,9 +78,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setString(@NonNull String name, String value) {
-        return setValue(name, value);
-    }
+    public Parameters setString(@NonNull String name, @Nullable String value) { return setValue(name, value); }
 
     /**
      * Set an Number value to the query parameter referenced by the given name. A query parameter
@@ -90,9 +89,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setNumber(@NonNull String name, Number value) {
-        return setValue(name, value);
-    }
+    public Parameters setNumber(@NonNull String name, @Nullable Number value) { return setValue(name, value); }
 
     /**
      * Set an int value to the query parameter referenced by the given name. A query parameter
@@ -103,9 +100,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setInt(@NonNull String name, int value) {
-        return setValue(name, value);
-    }
+    public Parameters setInt(@NonNull String name, int value) { return setValue(name, value); }
 
     /**
      * Set an long value to the query parameter referenced by the given name. A query parameter
@@ -116,9 +111,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setLong(@NonNull String name, long value) {
-        return setValue(name, value);
-    }
+    public Parameters setLong(@NonNull String name, long value) { return setValue(name, value); }
 
     /**
      * Set a float value to the query parameter referenced by the given name. A query parameter
@@ -129,9 +122,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setFloat(@NonNull String name, float value) {
-        return setValue(name, value);
-    }
+    public Parameters setFloat(@NonNull String name, float value) { return setValue(name, value); }
 
     /**
      * Set a double value to the query parameter referenced by the given name. A query parameter
@@ -142,9 +133,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setDouble(@NonNull String name, double value) {
-        return setValue(name, value);
-    }
+    public Parameters setDouble(@NonNull String name, double value) { return setValue(name, value); }
 
     /**
      * Set a boolean value to the query parameter referenced by the given name. A query parameter
@@ -155,9 +144,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setBoolean(@NonNull String name, boolean value) {
-        return setValue(name, value);
-    }
+    public Parameters setBoolean(@NonNull String name, boolean value) { return setValue(name, value); }
 
     /**
      * Set a date value to the query parameter referenced by the given name. A query parameter
@@ -168,9 +155,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setDate(@NonNull String name, Date value) {
-        return setValue(name, value);
-    }
+    public Parameters setDate(@NonNull String name, @Nullable Date value) { return setValue(name, value); }
 
     /**
      * Set the Blob value to the query parameter referenced by the given name. A query parameter
@@ -181,9 +166,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setBlob(@NonNull String name, Blob value) {
-        return setValue(name, value);
-    }
+    public Parameters setBlob(@NonNull String name, @Nullable Blob value) { return setValue(name, value); }
 
     /**
      * Set the Dictionary value to the query parameter referenced by the given name. A query parameter
@@ -194,9 +177,7 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setDictionary(@NonNull String name, Dictionary value) {
-        return setValue(name, value);
-    }
+    public Parameters setDictionary(@NonNull String name, @Nullable Dictionary value) { return setValue(name, value); }
 
     /**
      * Set the Array value to the query parameter referenced by the given name. A query parameter
@@ -207,30 +188,28 @@ public final class Parameters {
      * @return The self object.
      */
     @NonNull
-    public Parameters setArray(@NonNull String name, Array value) {
-        return setValue(name, value);
-    }
+    public Parameters setArray(@NonNull String name, @Nullable Array value) { return setValue(name, value); }
 
     /**
-     * Gets a parameter's value.
+     * Set a value to the query parameter referenced by the given name. A query parameter
+     * is defined by using the Expression's parameter(String name) function.
      *
-     * @param name The parameter name.
-     * @return The parameter value.
+     * @param name  The parameter name.
+     * @param value The value.
+     * @return The self object.
      */
-    public Object getValue(@NonNull String name) {
-        if (name == null) { throw new IllegalArgumentException("name cannot be null."); }
-        return map.get(name);
+    @NonNull
+    public Parameters setValue(@NonNull String name, @Nullable Object value) {
+        Preconditions.checkArgNotNull(name, "name");
+        if (readonly) { throw new IllegalStateException("Parameters is readonly mode."); }
+        map.put(name, value);
+        return this;
     }
 
     //---------------------------------------------
     // package level access
     //---------------------------------------------
-
-    Parameters readonlyCopy() {
-        final Parameters parameters = new Parameters(this);
-        parameters.readonly = true;
-        return parameters;
-    }
+    Parameters readonlyCopy() { return new Parameters(this, true); }
 
     AllocSlice encode() throws LiteCoreException {
         final FLEncoder encoder = new FLEncoder();
